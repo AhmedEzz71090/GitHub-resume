0fcd3dea9e9edca8aa2c8d7bd77b3d002cb6e29a
ahmed-k-ezz <ahmed01009344590>
2022-06-01 16:35:38 +0200

    Disable generate btn if input is empty

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {ResumeService} from '../resume.service';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-landing',
@@ -10,6 +11,7 @@ import {ResumeService} from '../resume.service';
 })
 export class LandingComponent implements OnInit {
   userName: string | undefined;
+  disabled = true;
 
   constructor(private router: Router, private http: HttpClient, private service: ResumeService) {
   }
@@ -29,5 +31,10 @@ export class LandingComponent implements OnInit {
 
   change(e: any): void {
     this.userName = e;
+    if (e) {
+      this.disabled = false;
+    } else  {
+      this.disabled = true;
+    }
   }
 }
